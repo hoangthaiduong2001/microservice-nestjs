3a12e95888f2f9a83a804e2300bdec4045fcf14b
Duong <duongtantien1@gmail.com>
2026-04-18 18:54:19 +0700

    chore(commintlint): set commitlint, husky, lintstaged

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import {PORT} from '@common/constants/common.constant'
+import { PORT } from '@common/constants/common.constant';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    console.log(PORT)
+    console.log(PORT);
     return { message: 'Hello API' };
   }
 }
