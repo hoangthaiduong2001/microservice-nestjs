ed253c68fadc2a4e7c119b1900be761dc63dda09
Duong <duongtantien1@gmail.com>
2026-04-23 21:45:58 +0700

    chore/#validate: setup validation dotenv

--- apps/bff/src/configuration/index.ts
+++ apps/bff/src/configuration/index.ts
@@ -1,10 +1,16 @@
 import { BaseConfiguration } from '@common/configuration/lib/base.config';
 import { AppConfiguration } from '@common/configuration/lib/app.config';
+import { ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
 class Configuration extends BaseConfiguration {
+  @ValidateNested()
+  @Type(() => AppConfiguration)
   APP_CONFIG: AppConfiguration = new AppConfiguration();
 }
 
 export const CONFIGURATION = new Configuration();
 
 export type TConfiguration = typeof CONFIGURATION;
+
+CONFIGURATION.validate();
--- libs/configuration/src/lib/base.config.ts
+++ libs/configuration/src/lib/base.config.ts
@@ -1,13 +1,38 @@
+import { Logger } from '@nestjs/common';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  validate,
+  validateSync,
+} from 'class-validator';
+
 export class BaseConfiguration {
+  @IsString()
   NODE_ENV: string;
 
+  @IsBoolean()
   IS_DEV: boolean;
 
+  @IsString()
+  @IsNotEmpty()
   GLOBAL_PREFIX: string;
 
   constructor() {
     this.NODE_ENV = process.env['NODE_ENV'] || 'development';
     this.IS_DEV = this.NODE_ENV === 'development';
-    this.GLOBAL_PREFIX = process.env['GLOBAL_PREFIX'] || 'api/v1';
+    this.GLOBAL_PREFIX = process.env['GLOBAL_PREFIX'] || '';
+  }
+
+  validate() {
+    const errors = validateSync(this);
+    if (errors.length > 0) {
+      const _errors = errors.map((err) => {
+        return err.children;
+      });
+
+      Logger.error(_errors);
+      throw new Error('Configuration validation failed');
+    }
   }
 }
