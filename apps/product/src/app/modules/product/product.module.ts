c91ca94fe4b6c094dc3e484f5c43a14bb8695b3b
FE-DUONG <rd_duong@bpo-it.net>
2026-06-03 17:09:05 +0700

    feat/#handle_register_entities: register entities product

--- apps/product/src/app/modules/product/product.module.ts
+++ apps/product/src/app/modules/product/product.module.ts
@@ -1,8 +1,10 @@
 import { TypeOrmProvider } from '@common/configuration/lib/type-orm.config';
+import { Product } from '@common/entities/product.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmProvider],
+  imports: [TypeOrmProvider, TypeOrmModule.forFeature([Product])],
   exports: [],
   controllers: [],
   providers: [],
